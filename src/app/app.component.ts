432a9902b102aef6cf43dc751a32beda0a9f0f95
An Ngo <annc.21198@gmail.com>
2025-06-12 22:59:20 +0700

    fix: convention

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,11 +21,11 @@ export class AppComponent implements AfterViewInit {
 	ngAfterViewInit(): void {
 		if ( isPlatformBrowser( this.platformId ) ) {
 			const togtop = () =>
-				document.getElementById('backtop').style.display
+				document.getElementById( 'backtop' ).style.display
 					= window.scrollY >= 800 ? 'block' : 'none';
 
-			window.addEventListener('scroll', togtop);
-			window.addEventListener('resize', togtop);
+			window.addEventListener( 'scroll', togtop );
+			window.addEventListener( 'resize', togtop );
 		}
 	}
 }
